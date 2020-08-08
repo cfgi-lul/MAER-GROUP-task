857cbfc8113d33816e0f07edd9fd2fa2d4b0bf9b
cfgi <cfgi@yandex.ru>
2020-08-08 18:16:49 +0500

    Отображение по таблицам + валидация

--- src/app/mac-address-validation/mac-address-validation.component.ts
+++ src/app/mac-address-validation/mac-address-validation.component.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {MacServiceService} from '../macService.service';
-import {add} from 'ngx-bootstrap/chronos';
 
 
 @Component({
@@ -12,21 +11,28 @@ import {add} from 'ngx-bootstrap/chronos';
 export class MacAddressValidationComponent {
   private macAddress: string;
   public macAddressInfo: Array<any>;
+  public macRegex: RegExp;
 
   constructor(private http: HttpClient, private macService: MacServiceService) {
+    this.macRegex = new RegExp('^[\\da-f]{2}:[\\da-f]{2}:[\\da-f]{2}:[\\da-f]{2}:[\\da-f]{2}:[\\da-f]{2}$', 'gsi');
     this.macAddressInfo = new Array<any>();
     this.macService.sharedMessage.subscribe(addresses => this.macAddressInfo = addresses);
     this.macAddress = '';
   }
 
+  macValidate(mac) {
+    return this.macRegex.test(mac) && this.macAddressInfo.filter(e => e.macAddressDetails.searchTerm === mac).length === 0;
+  }
 
   requestMacInfo(macAddress) {
-    console.log(this.macAddressInfo);
-    const data = this.http
-      .get('https://api.macaddress.io/v1?apiKey=at_BULmXFODqtiqHIVJ6CERtQ11sBwzn&output=json&search=' + macAddress);
-    data.subscribe(e => {
-      this.macAddressInfo.push(e);
-      this.macService.nextArray(this.macAddressInfo);
-    });
+    if (this.macValidate(macAddress)) {
+      this.macValidate(macAddress);
+      const data = this.http
+        .get('https://api.macaddress.io/v1?apiKey=at_BULmXFODqtiqHIVJ6CERtQ11sBwzn&output=json&search=' + macAddress);
+      data.subscribe(e => {
+        this.macAddressInfo.push(e);
+        this.macService.nextArray(this.macAddressInfo);
+      });
+    }
   }
 }
